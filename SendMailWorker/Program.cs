945b1f34d3d6d50624a0b1c0acfd2133858540f1
Jarle Nygård <jany@powel.com>
2019-09-10 13:27:11 +0200

    Add project files.

---
+++ SendMailWorker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SendMailWorker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
